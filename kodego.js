61c761252b2ed2bcee3c5465f3643f4288e2f0fb
astraa92831 <119955056+astraa92831@users.noreply.github.com>
2022-12-16 21:03:35 +0800

    Add files via upload

---
+++ kodego.js
@@ -0,0 +1,22 @@
+//control nlng po sir kung mag papalit po kayo age dito sa var age tapos run po ulit 
+var age = 10;
+
+    if  (age>= 60) {
+      console.log("Senior Adult");
+    } else if  (age>= 40) {
+      console.log("Middle age Adult");
+    } else if  (age>= 20) {
+      console.log("Adult");
+    }else if  (age>= 13) {
+      console.log("Teen");
+    }else if  (age>=5) {
+      console.log("Child");
+    }
+    else if  (age>= 2) {
+      console.log("Todler");
+    }
+    else if  (age>= 0) {
+      console.log("Infant");
+    };
+
+
